3eee93521e681526227da944ca24cdc65af4e010
Manu <mcufre84@gmail.com>
2021-07-06 23:35:41 +0200

    fix viewport metatag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,8 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Web-developer portfolio for Manuel Cufré</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Theme>
         <Component {...pageProps} />
