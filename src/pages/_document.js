344af8391e6ca32ac5c2375bc8365ffd37466afe
Manu <mcufre84@gmail.com>
2021-07-07 00:26:27 +0200

    add social img for whatsapp

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -56,6 +56,7 @@ export default class MyDocument extends Document {
             property="og:image"
             content="https://raw.githubusercontent.com/ManuC84/portfolio_website/master/public/images/social-image.png"
           />
+
           <link
             href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
